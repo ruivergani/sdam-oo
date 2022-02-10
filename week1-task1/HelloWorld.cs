5456d4dc36640fa8d4816910654891dd692115c7
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-02-10 17:30:13 +0000

    exerc1exercise 1

---
+++ week1-task1/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace week1_task1
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name?");
+            string name = Console.ReadLine(); /* declare variable and get data from user */
+            Console.WriteLine("\n \tHello {0}", name);
+
+            Console.Write("\nWhat is your age? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n \t Hello {0}, you are {1} years old", name, age);
+        }
+    }
+}
