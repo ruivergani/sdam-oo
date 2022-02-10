226d8e1e38f08b690443cdf55125b98a3bcd370e
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-02-10 17:30:13 +0000

    exercise 1

--- week1-helloworld/Program.cs
+++ week1-helloworld/Program.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using System; /* same as import in python */
 
-namespace week1_helloworld
+namespace week1_helloworld /* namespace is a collection of classes */
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) /* main method is the first one to execute */
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); /* output statement */
             Console.WriteLine("Rui good afternoon.");
+
+            Console.WriteLine("\n \t'{0}'", "Hello again");
+            Console.Write("\n\t\t\t'{0, 20}'\n", "Hello once more");
         }
     }
 }
