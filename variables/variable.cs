79f471fecb604afe66354000d965c42fcb1b7ca3
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-02-10 18:14:10 +0000

    changes exercises

---
+++ variables/variable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Rui Vergani Neto"; /* store text */
+            int age = 21; /* whole number */
+            int characterAge; /* just declaring variable */
+            characterAge = 35;
+            Console.WriteLine("There once was a men named {0}", characterName); /* concatenate */
+            Console.WriteLine("Your name is: " + characterName); /* concatenate */
+            Console.WriteLine("He was " + characterAge + " years old.");
+        }
+    }
+}
