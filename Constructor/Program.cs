b743369dfd5cc5c0588a6d12a306f1713a63b942
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-02-17 11:11:25 +0000

    Constructor concept

---
+++ Constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Constructor
+{
+    // Create a Car class
+    class Car
+    {
+        public string model;  // Create a field
+
+        // Create a class constructor for the Car class
+        public Car()
+        {
+            model = "Mustang"; // Set the initial value for model
+        }
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car();  // Create an object of the Car Class (this will call the constructor)
+            Console.WriteLine(Ford.model);  // Print the value of model
+        }
+    }
+
+    // Outputs "Mustang"
+}
