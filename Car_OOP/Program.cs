058514a800e53b48440edfde75b2b508e1f4ee3e
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-02-17 10:29:44 +0000

    Car OO exercise

---
+++ Car_OOP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Car_OOP
+{
+    class Car // class declaration (Car class)
+    {
+        string model;
+        string color;
+        int year;
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car(); // declaring object called Ford
+            Ford.color = "red";
+            Ford.model = "Mustang";
+            Ford.year = 2021;
+
+            Car Opel = new Car();
+            Opel.model = "Astra";
+            Opel.color = "white";
+
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Opel.color);
+
+        }
+    }
+}
