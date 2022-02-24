7ad6173a03bd43efed3a0b892b045656e900aaec
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-02-24 15:53:04 +0000

    for loop array and int variable

---
+++ forLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace forLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Loop exercise example.");
+
+            for(int i=0; i < 5; i++) // for loop example
+            {
+                Console.WriteLine("i: " + i);
+            }
+
+            //printing array of lucky numbers using for loop
+            int[] luckyNumbers = { 1, 2, 3, 4, 5, 6 }; 
+            for (int i = 0; i <= luckyNumbers.Length; i++)
+            {
+                Console.WriteLine(luckyNumbers[i]);
+            }
+        }
+    }
+}
