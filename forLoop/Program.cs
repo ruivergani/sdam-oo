1b3f8670892390d76e8c4511b4c33150a36da303
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-02-24 15:54:14 +0000

    for loop

--- forLoop/Program.cs
+++ forLoop/Program.cs
@@ -15,7 +15,7 @@ namespace forLoop
 
             //printing array of lucky numbers using for loop
             int[] luckyNumbers = { 1, 2, 3, 4, 5, 6 }; 
-            for (int i = 0; i <= luckyNumbers.Length; i++)
+            for (int i = 0; i < luckyNumbers.Length; i++)
             {
                 Console.WriteLine(luckyNumbers[i]);
             }
