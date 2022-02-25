6e268df1107b5c26910a98b5c7c614e7a7658c47
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-02-25 18:09:57 +0000

    exception handling with loop

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -6,18 +6,29 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
+            bool trueval = false;
+            do
+            {
+                try
+                {
+                    Console.Write("\nEnter a number: ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter another number: ");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    int result = (num1 / num2);
+                    Console.WriteLine("Resultado: " + result);
+                    trueval = false;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message); //catch the error and display a message
+                    trueval = true;
+                }
+            }
+            while (trueval == true);
             // exception block
-            try {
-                Console.Write("Enter a number: ");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter another number: ");
-                int num2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine(num1 / num2);
-            }
-            catch (Exception e){
-                Console.WriteLine(e.Message); //catch the error and display a message
-
-            }
+            trueval = false;
+            
         }
     }
 }
