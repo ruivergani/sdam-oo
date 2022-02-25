81ad976c3d48fd9990b5514465825ca0e230fa6f
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-02-25 15:02:16 +0000

    creating 2D array

--- ExponentMethod/Program.cs
+++ ExponentMethod/Program.cs
@@ -8,6 +8,19 @@ namespace ExponentMethod
         {
             Console.WriteLine("Exponent Function");
             Console.WriteLine(GetPow(3, 2)); // function get power 3 ^ 2
+
+            // creating 2D array
+            int[,] numberGrid = {
+                {1, 2},
+                {3, 4},
+                {5, 6}
+            };
+            Console.WriteLine(numberGrid[0, 0]); // accessing values
+            Console.WriteLine(numberGrid[1, 1]);
+
+            int[,] myArray = new int[2, 3]; // rows and collumns (because you do not know the elements) 
+
+
         }
         // creating function
         // basically the function does not need to be before the main method
