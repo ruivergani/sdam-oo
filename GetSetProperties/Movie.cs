6bc21dc8e6c3df2bcd0869dd6a1604920f474dd6
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-02-25 23:14:55 +0000

    get and set of each attribute

---
+++ GetSetProperties/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetSetProperties
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating; // in order to use the get and set
+
+        // the idea is to create get/set for every single attribute in order to manipulate and secure the system
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating; // calling the set
+        }
+        // defining get/set
+        public string Rating
+        {
+            get {
+                return rating;
+            }
+            set {
+                // example of Non-standard set method
+                if (value == "G" || value == "PG") // only set the value to the attribute if satisfy the condition
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "Not declared.";
+                }
+            }
+        }
+    }
+}
---
+++ GetSetProperties/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetSetProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie avengers = new Movie("The Avengers", "Joss wilson", "PG");
+        }
+    }
+}
