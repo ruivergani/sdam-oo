22f054e2935e59f59a229436aed0c1f752245c4d
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-02-25 23:51:39 +0000

    static learning concept

---
+++ StaticANDthis/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaticANDthis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song holiday = new Song("Holiday", "Green Day", 200);
+            Console.WriteLine(Song.songCount); // I can only call here because of being object of the class - STATIC
+            // Console.WriteLine(Song.artist); does not work without using an object
+            // Console.WriteLine is independant of the object you do not need to create an object to use a method of the class Console
+        }
+    }
+}
---
+++ StaticANDthis/Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticANDthis
+{
+    class Song
+    {
+        // static means that belongs to the class not to the object (about the CLASS not OBJECT)
+        public string title;
+        public string artist;
+        public int duration;
+        public static int songCount = 0; // common to all the class
+
+        public Song(string aTitle, string aArtist, int aDuration) // basic constructor
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+            songCount++; // increment the static attribute of the class Song
+        }
+    }
+}
