86c23695c25274b44344bdcf109075f6edd78985
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-02-25 22:23:08 +0000

    object methods

---
+++ ObjectMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ObjectMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Jim", "Business", 3.4);
+
+            Console.WriteLine(student1.HasHonors()); // calling the object method
+        }
+    }
+}
---
+++ ObjectMethods/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectMethods
+{
+    class Student
+    {
+        public string major;
+        public double gpa;
+        public string name;
+
+        public Student(string aName, string aMajor, double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;  
+        }
+        public bool HasHonors()
+        {
+            if(gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
