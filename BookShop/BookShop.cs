dcf3562aec249ff14b40de41a6f073f16886b18f
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-02-28 22:17:16 +0000

    add book method done

--- BookShop/BookShop.cs
+++ BookShop/BookShop.cs
@@ -8,9 +8,10 @@ namespace BookShop
     {
         public List<Book> AvailableSales { get; set; } //Book objects that are available for sale
         public List<Book> SoldSales { get; set; } //Book objects that have been sold
-
-        public BookShop()
+        public string name;
+        public BookShop(string aName)
         {
+            name = aName;
             AvailableSales = new List<Book>(); // creating object reference list
             SoldSales = new List<Book>();
         }
@@ -19,16 +20,24 @@ namespace BookShop
         public void addBook(Book b)  //add Book for-sale list
         {
             AvailableSales.Add(b);
+            Console.WriteLine("Added item!");
+        }
+
+        public void sellBook(Book a) // remove book for-sale and add to sold-list
+        {
+            AvailableSales.Remove(a);
+            SoldSales.Add(a);
             Console.WriteLine("Items on for-sale list: " + AvailableSales.Count);
+            Console.WriteLine("Items on sold-sale list: " + SoldSales.Count);
         }
-
-        public void sellBook() // remove book for-sale and add to sold-list
+        public void reportSales(Book c) //add price tags of all books in sold lists
         {
-
-        }
-        public void reportSales() //add price tags of all books in sold lists
-        {
-
+            double sum = 0;
+            foreach(var item in SoldSales)
+            {
+                Console.WriteLine("List regarding price tag: " + c.priceTag);
+                sum += c.priceTag;
+            }
         }
 
 
--- BookShop/Program.cs
+++ BookShop/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BookShop
 {
@@ -6,12 +7,22 @@ namespace BookShop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Book Shop");
+            Console.WriteLine("Book Shop! ");
 
             Book javaBook = new Book("Java is Ace!", "Graham Mansfield", 30.50);
             Book cSharpBook = new Book("C# is good!", "Jan Lawton", 10);
             Book pythonBook = new Book("What is Python?", null, 20.30);
             Book programmingBook = new Book("", "Kelvin Hilton", 30.99);
+
+            BookShop library01 = new BookShop("Wilson Library"); // BookShop Object
+
+            library01.addBook(javaBook);
+            library01.addBook(cSharpBook);
+            library01.addBook(pythonBook);
+            library01.addBook(programmingBook);
+            Console.WriteLine("Items on for-sale list: " + library01.AvailableSales.Count);
+
+
 
         }
 
