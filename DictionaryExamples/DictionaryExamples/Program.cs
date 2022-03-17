4bfd2d1224d06ae80e283eaea04441fadd88342f
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-03-17 18:31:25 +0000

    DICTIONARY CONCEPT

--- DictionaryExamples/DictionaryExamples/Program.cs
+++ DictionaryExamples/DictionaryExamples/Program.cs
@@ -7,8 +7,9 @@ namespace DictionaryExamples
     {
         static void Main(string[] args)
         {
+            // DICTIONARY IN C#
             Dictionary<int, Person> dict = new Dictionary<int, Person>();
-            SortedDictionary<int, Person> sDict = new SortedDictionary<int, Person>();
+            SortedDictionary<int, Person> sDict = new SortedDictionary<int, Person>(); // sort by values
 
             for (int i = 0; i < 5; i++)
             {
@@ -23,18 +24,18 @@ namespace DictionaryExamples
                     Name = name
                 };
 
-                dict.Add(id, p);
+                dict.Add(id, p); //adding person p with this key (id) to the values p(id and name)
                 sDict.Add(id, p);
             }
 
             Console.WriteLine("\nContents of the dictionary:");
-            foreach (Person p in dict.Values)
+            foreach (Person p in dict.Values) // GET VALUES
             {
                 Console.WriteLine("\t" + p);
             }
 
             Console.WriteLine("\nContents of the sorted dictionary:");
-            foreach (Person p in sDict.Values)
+            foreach (Person p in sDict.Values) // GET SORTED VALUES
             {
                 Console.WriteLine("\t" + p);
             }
--- ListExample/ListExample/Program.cs
+++ ListExample/ListExample/Program.cs
@@ -7,24 +7,25 @@ namespace ListExample
     {
         static void Main(string[] args)
         {
+            // Creating a List
             List<int> list = new List<int>();
 
             for (int i = 0; i < 5; i++)
             {
                 Console.Write("Enter integer {0}: ", i+1);
                 //list.Add(Console.ReadLine());
-                list.Add(Convert.ToInt32(Console.ReadLine())); // add
+                list.Add(Convert.ToInt32(Console.ReadLine())); // add to the List
             }
 
             Console.WriteLine("\nContents of the list:");
             Console.Write("\t");
-            foreach (int item in list)
+            foreach (int item in list) // reading item from list 
             {
                 Console.Write("{0}  ", item);
             }
 
-            Console.WriteLine("\n\nThe list contains {0} items", list.Count);
-            Console.WriteLine("\nThe list contains 10: {0}", list.Contains(10));
+            Console.WriteLine("\n\nThe list contains {0} items", list.Count);//List counter
+            Console.WriteLine("\nThe list contains 10: {0}", list.Contains(10)); // true or false - comparing the value
             Console.WriteLine("\nThe list contains 12: {0}", list.Contains(12));
 
             Console.WriteLine("\nRemove 12");
@@ -39,7 +40,7 @@ namespace ListExample
                 Console.Write("{0}  ", item);
             }
 
-            list.Sort(); // sort
+            list.Sort(); // sort item
             Console.WriteLine("\n\nContents of the list after sorting:");
             Console.Write("\t");
             foreach (int item in list)
